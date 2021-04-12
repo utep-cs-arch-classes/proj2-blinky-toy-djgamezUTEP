ebbbcf2b0a72c9dd975eb9c8ee4cd3c7ccf9fdcc
djgamezUTEP <djgamez@miners.utep.edu>
2021-04-12 09:13:54 -0600

    changed stateMachines to include 0%, 50%, 75% and 100% brightness added stateMachines.c

--- project/stateMachines.c
+++ project/stateMachines.c
@@ -1,7 +1,14 @@
 #include <msp430.h>
 #include "stateMachines.h"
 #include "led.h"
-
+void red_on_ctrl(){
+  red_on=1;
+  led_changed=1;  
+}
+void red_off_ctrl(){
+  red_on=0;
+  led_changed=1;
+}
 char toggle_red()		/* always toggle! */
 {
   static char state = 0;
@@ -22,27 +29,88 @@ char toggle_red()		/* always toggle! */
 char toggle_green()	/* only toggle green if red is on!  */
 {
   char changed = 0;
-  if (red_on) {
+  if (red_on==0) {
     green_on ^= 1;
     changed = 1;
   }
   return changed;
 }
 
+void med_dim(){
+  static char cntr=0;
+    if(cntr%2==0){
+      red_off_ctrl();
 
-void state_advance()		/* alternate between toggling red & green */
+    }else{
+      red_on_ctrl();
+    }
+  cntr+=1;
+  if (cntr>100){
+    cntr=0;
+  }
+  
+}
+void med_high_dim(){
+  static char cntr=0;
+  if(cntr>2){
+    red_off_ctrl();
+  }else{
+    red_on_ctrl();
+  }
+  cntr+=1;
+  if (cntr>3){
+    cntr=0;
+  }
+}
+
+
+
+static char dimLvl=0;
+void change_dim(){
+  dimLvl+=1;
+  if( dimLvl>4){
+    dimLvl=0;
+    }
+}
+void dimming()
 {
-  char changed = 0;  
+  static char cntr=0;
+  
+  switch(dimLvl){
+  case 0:  red_off_ctrl();//off all the time
+    break;
+  case 2: med_dim();
+    break;
+  case 3: med_high_dim();
+    break;
+  case 4: red_on_ctrl();
+    break;
+    
 
-  static enum {R=0, G=1} color = G;
-  switch (color) {
-  case R: changed = toggle_red(); color = G; break;
-  case G: changed = toggle_green(); color = R; break;
   }
 
-  led_changed = changed;
-  led_update();
 }
+  
+/*void state_advance()		/* alternate between toggling red & green 
+{
+  char changed = 0;
+  static char subState=0;
+  switch(subState){
+  case 0:changed= toggle_red();
+    subState+=1;
+    break;
+  case 1: changed= toggle_red(); susbState+=1;
+   
+  
+  /* static enum {R=0, G=1} color = G;
+  switch (color) {
+  case R: changed = toggle_red(); color = G; break;
+  case G: changed = toggle_green(); color = R; break;*/
+  
+
+  //led_changed = changed;
+//led_update();
+//}
 
 
 
--- project/wdInterruptHandler.c
+++ project/wdInterruptHandler.c
@@ -3,9 +3,22 @@
 
 void
 __interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
+  static int state_count=0;
   static char blink_count = 0;
-  if (++blink_count == 125) {
-    state_advance();
+  if (++blink_count ==350) {
+    
     blink_count = 0;
+    
   }
+  if(++state_count==225){
+    change_dim();
+    
+    
+    state_count=0;
+    
+   
+  }
+  dimming();
+  led_update();
+
 }
